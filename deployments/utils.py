3c4f81036d52199e442e374a257bacddf9d0512e
AhmedSheri <85445168+AhmedSheri123@users.noreply.github.com>
2025-09-14 12:54:20 +0300

    aa

--- deployments/utils.py
+++ deployments/utils.py
@@ -202,6 +202,7 @@ def run_docker(deployment: Deployment, plan: Plan):
             image=image_name,
             name=container_name,
             labels=labels,
+            ports={"8000/tcp": port},
             detach=True,
             mem_limit=mem_limit,
             cpu_quota=cpu_quota,
